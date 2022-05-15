b1fa7f2ab3a409528707d3795635e70697e4b4ca
Nicolas DUBIEN <github@dubien.org>
2022-05-15 21:18:17 +0200

    Migrate App2

--- render-as-you-fetch/src/App2.tsx
+++ render-as-you-fetch/src/App2.tsx
@@ -1,9 +1,18 @@
 // Pretty close to App1 but enabling render-as-you-fetch capabilities.
 // It still performs the same way in terms of requests but the loading states
-// are done handled via the parents through Suspense
+// are done handled via the parents through Suspense.
+// Not optimal as Suspense (outside of the Component loading stuff) tend to recreate
+// a placeholder version of the Component.
 
 import { Suspense } from "react";
-import { retrieveTeam, retrieveMemberName } from "./api/Fetcher";
+import {
+  retrieveTeam,
+  retrieveMemberName,
+  retrieveCurrentUser,
+  Team,
+  retrieveNews,
+} from "./api/Fetcher";
+import Card from "./Card";
 import { useRenderAsYouFetch } from "./RenderAsYouFetch";
 
 type Props = { teamNumber: number };
@@ -23,30 +32,53 @@ function Page(props: PageProps) {
 
   return (
     <div>
-      <p>
-        <b>Team:</b>
-      </p>
-      <p>Admin:</p>
+      <Suspense fallback={<Card>Please wait...</Card>}>
+        <Header teamNumber={props.teamNumber} team={team.get()} />
+      </Suspense>
+      <Content team={team.get()} />
+      <Suspense fallback={<Card>Please wait...</Card>}>
+        <Footer teamNumber={props.teamNumber} />
+      </Suspense>
+    </div>
+  );
+}
+
+type PropsHeader = {
+  teamNumber: number;
+  team: Team;
+};
+
+function Header(props: PropsHeader) {
+  const currentUser = useRenderAsYouFetch(retrieveCurrentUser, [
+    props.teamNumber,
+  ]);
+
+  return (
+    <Card>
+      <p>Welcome {currentUser.get()}</p>
+      <p>Team name: {props.team.teamName}</p>
+    </Card>
+  );
+}
+
+type PropsContent = {
+  team: Team;
+};
+
+function Content(props: PropsContent) {
+  return (
+    <Card>
+      Team members are:
       <ul>
-        <Suspense fallback={<li>Please wait...</li>}>
-          <Member id={`id:${props.teamNumber + 1000}`} />
-        </Suspense>
-      </ul>
-      <p>Members:</p>
-      <ul>
-        {team.get().members.map((userId) => (
-          <Suspense key={userId} fallback={<li>Please wait...</li>}>
-            <Member id={userId} />
-          </Suspense>
+        {props.team.members.map((memberId) => (
+          <li key={memberId}>
+            <Suspense fallback={"Please wait..."}>
+              <Member id={memberId} />
+            </Suspense>
+          </li>
         ))}
       </ul>
-      <p>Referee:</p>
-      <ul>
-        <Suspense fallback={<li>Please wait...</li>}>
-          <Member id={`id:${props.teamNumber + 2000}`} />
-        </Suspense>
-      </ul>
-    </div>
+    </Card>
   );
 }
 
@@ -56,8 +88,27 @@ function Member(props: PropsMember) {
   const name = useRenderAsYouFetch(retrieveMemberName, [props.id]);
 
   return (
-    <li>
+    <span>
       id({props.id}) =&gt; {name.get()}
-    </li>
+    </span>
+  );
+}
+
+type PropsFooter = {
+  teamNumber: number;
+};
+
+function Footer(props: PropsFooter) {
+  const news = useRenderAsYouFetch(retrieveNews, [props.teamNumber]);
+
+  return (
+    <Card>
+      Got new regarding your favourite team:
+      <ul>
+        {news.get().map((n, i) => (
+          <li key={i}>{n}</li>
+        ))}
+      </ul>
+    </Card>
   );
 }
