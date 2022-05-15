15c1aeaa5ea12f2782ff548e8e19355f66cd2936
Nicolas DUBIEN <github@dubien.org>
2022-05-15 21:38:42 +0200

    Yet another version

--- render-as-you-fetch/src/App.tsx
+++ render-as-you-fetch/src/App.tsx
@@ -4,8 +4,9 @@ import App2 from "./App2";
 import App3 from "./App3";
 import App4 from "./App4";
 import App5 from "./App5";
+import App6 from "./App6";
 
-const KnownTypes = [App1, App2, App3, App4, App5];
+const KnownTypes = [App1, App2, App3, App4, App5, App6];
 
 function App() {
   const [teamNumber, setTeamNumber] = useState(0);
---
+++ render-as-you-fetch/src/App6.tsx
@@ -0,0 +1,107 @@
+// App5 with more restricted props
+
+import { Suspense } from "react";
+import {
+  retrieveTeam,
+  retrieveMemberName,
+  retrieveCurrentUser,
+  Team,
+  retrieveNews,
+} from "./api/Fetcher";
+import Card from "./Card";
+import { AsYouFetch, useRenderAsYouFetch } from "./RenderAsYouFetch";
+
+type Props = { teamNumber: number };
+
+export default function App(props: Props) {
+  return (
+    <Suspense fallback={<div>Please wait...</div>}>
+      <Page {...props} />
+    </Suspense>
+  );
+}
+
+type PageProps = { teamNumber: number };
+
+function Page(props: PageProps) {
+  const team = useRenderAsYouFetch(retrieveTeam, [props.teamNumber]);
+
+  return (
+    <div>
+      <Header
+        teamNumber={props.teamNumber}
+        teamName={team.derive((t) => t.teamName)}
+      />
+      <Content members={team.derive((t) => t.members)} />
+      <Footer teamNumber={props.teamNumber} />
+    </div>
+  );
+}
+
+type PropsHeader = {
+  teamNumber: number;
+  teamName: AsYouFetch<Team["teamName"]>;
+};
+
+function Header(props: PropsHeader) {
+  const currentUser = useRenderAsYouFetch(retrieveCurrentUser, [
+    props.teamNumber,
+  ]);
+
+  return (
+    <Card>
+      <p>Welcome {currentUser.get()}</p>
+      <p>Team name: {props.teamName.get()}</p>
+    </Card>
+  );
+}
+
+type PropsContent = {
+  members: AsYouFetch<Team["members"]>;
+};
+
+function Content(props: PropsContent) {
+  return (
+    <Card>
+      Team members are:
+      <ul>
+        {props.members.get().map((memberId) => (
+          <li key={memberId}>
+            <Member id={memberId} />
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+}
+
+type PropsMember = { id: string };
+
+function Member(props: PropsMember) {
+  const name = useRenderAsYouFetch(retrieveMemberName, [props.id]);
+
+  return (
+    <span>
+      id({props.id}) =&gt; {name.get()}
+    </span>
+  );
+}
+
+type PropsFooter = {
+  teamNumber: number;
+};
+
+function Footer(props: PropsFooter) {
+  const news = useRenderAsYouFetch(retrieveNews, [props.teamNumber]);
+
+  return (
+    <Card>
+      Got new regarding your favourite team:
+      <ul>
+        {news.get().map((n, i) => (
+          <li key={i}>{n}</li>
+        ))}
+      </ul>
+    </Card>
+  );
+}
