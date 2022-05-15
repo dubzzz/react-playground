7caad3014f1f4e6bfa472ca8e35d01d7bb6db013
Nicolas DUBIEN <github@dubien.org>
2022-05-15 21:41:04 +0200

    Better render

--- render-as-you-fetch/src/App.tsx
+++ render-as-you-fetch/src/App.tsx
@@ -5,8 +5,9 @@ import App3 from "./App3";
 import App4 from "./App4";
 import App5 from "./App5";
 import App6 from "./App6";
+import App7 from "./App7";
 
-const KnownTypes = [App1, App2, App3, App4, App5, App6];
+const KnownTypes = [App1, App2, App3, App4, App5, App6, App7];
 
 function App() {
   const [teamNumber, setTeamNumber] = useState(0);
