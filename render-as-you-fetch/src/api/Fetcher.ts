30930d55f7b70429e4467001b22d9b1e22e07e5d
Nicolas DUBIEN <github@dubien.org>
2022-05-15 12:37:13 +0200

    Better counter for fetch

--- render-as-you-fetch/src/api/Fetcher.ts
+++ render-as-you-fetch/src/api/Fetcher.ts
@@ -1,20 +1,26 @@
-let numQueries = 0;
+const numQueries: { [key: string]: number } = {};
 
 function updateCounter() {
   const counterDiv = document.getElementById("counter");
   if (!counterDiv) {
     return;
   }
-  counterDiv.innerText = `${numQueries} running`;
+  counterDiv.innerText = `${Object.values(numQueries).reduce(
+    (a, b) => a + b,
+    0
+  )} running (${Object.entries(numQueries)
+    .filter((query) => query[1] > 0)
+    .map((query) => `${query[0]}: ${query[1]}`)
+    .join(", ")})`;
 }
 
-function delay<T>(p: Promise<T>): Promise<T> {
-  ++numQueries;
+function delay<T>(p: Promise<T>, type: string): Promise<T> {
+  numQueries[type] = (numQueries[type] || 0) + 1;
   updateCounter();
   return new Promise<T>((resolve) => {
     setTimeout(() => resolve(p), 5000 + Math.random() * 5000);
   }).finally(() => {
-    --numQueries;
+    --numQueries[type];
     updateCounter();
   });
 }
@@ -22,13 +28,15 @@ function delay<T>(p: Promise<T>): Promise<T> {
 export function retrieveUserIds(cb: number): Promise<string[]> {
   console.info(`START: retrieveUserIds(${cb})`);
   return delay(
-    Promise.resolve([...Array(4)].map((_, i) => "id:" + String(cb + i)))
+    Promise.resolve([...Array(4)].map((_, i) => "id:" + String(cb + i))),
+    "retrieveUserIds"
   ).finally(() => console.info(`END: retrieveUserIds(${cb})`));
 }
 
 export function retrieveUserName(userId: string): Promise<string> {
   console.info(`START: retrieveUserName(${userId})`);
-  return delay(Promise.resolve(`User ${userId.substring(3)}`)).finally(() =>
-    console.info(`END: retrieveUserName(${userId})`)
-  );
+  return delay(
+    Promise.resolve(`User ${userId.substring(3)}`),
+    "retrieveUserName"
+  ).finally(() => console.info(`END: retrieveUserName(${userId})`));
 }
