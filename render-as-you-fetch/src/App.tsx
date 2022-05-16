af16b6a16af77996838df54d6092a92d88c0ebb0
Nicolas DUBIEN <github@dubien.org>
2022-05-16 08:30:04 +0200

    Add Hello message

--- render-as-you-fetch/src/App.tsx
+++ render-as-you-fetch/src/App.tsx
@@ -8,7 +8,7 @@ import App6 from "./App6";
 import App7 from "./App7";
 import App8 from "./App8";
 
-const KnownTypes = [App1, App2, App3, App4, App5, App6, App7, App8];
+const KnownTypes = [Hello, App1, App2, App3, App4, App5, App6, App7, App8];
 
 function App() {
   const [teamNumber, setTeamNumber] = useState(0);
@@ -27,11 +27,17 @@ function App() {
           }}
           style={type === index ? { border: "1px solid red" } : {}}
         >
-          {index + 1}
+          {index}
         </button>
       ))}{" "}
       — Current is {teamNumber} — <span id="counter"></span>
-      <Br />
+      <div
+        style={{
+          borderBottom: "1px black solid",
+          marginTop: "16px",
+          marginBottom: "16px",
+        }}
+      ></div>
       {<SelectedApp teamNumber={teamNumber} />}
     </div>
   );
@@ -39,14 +45,22 @@ function App() {
 
 export default App;
 
-function Br() {
+function Hello() {
   return (
-    <div
-      style={{
-        borderBottom: "1px black solid",
-        marginTop: "16px",
-        marginBottom: "16px",
-      }}
-    ></div>
+    <div>
+      <p>
+        <b>Welcome in my "render-as-you-fetch" playground</b>
+      </p>
+      <p>
+        The aim is mostly to play around the feature not to implement THE
+        perfect render-as-you-fetch patterns and helpers.
+        <br />
+        You can play with various iterations I made around the topic: from
+        legacy way to handle requests to render-as-you-fetch.
+        <br />
+        Just press one of the buttons above to run one of the snippets: button{" "}
+        <code>1</code> is for <code>App1.tsx</code>...
+      </p>
+    </div>
   );
 }
